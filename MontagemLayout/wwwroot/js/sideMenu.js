79a15df579766276844100b6280d48f4f9cdd778
malic15 <matheus7184@hotmail.com>
2025-07-03 15:53:48 -0300

    Backup automÃ¡tico em 2025-07-03 15:53:48

--- MontagemLayout/wwwroot/js/sideMenu.js
+++ MontagemLayout/wwwroot/js/sideMenu.js
@@ -1,4 +1,6 @@
-﻿document.getElementById("sideMenuOpenBtn").onclick = function() {
+﻿import { setReplayMode } from '/js/mqttWebSocket.js';
+
+document.getElementById("sideMenuOpenBtn").onclick = function () {
     document.getElementById("sideMenu").classList.add("open");
 };
 document.getElementById("sideMenuCloseBtn").onclick = function() {
@@ -14,4 +16,23 @@ document.addEventListener("mousedown", function(e){
     ) {
         sideMenu.classList.remove("open");
     }
+});
+
+document.querySelectorAll('.side-menu-item').forEach(item => {
+    item.addEventListener('click', function (e) {
+        var isReplayActive = false;
+        // Remove a classe 'active' de todos
+        document.querySelectorAll('.side-menu-item').forEach(el => {
+            if (el.textContent.trim().includes('Replay') && el.classList.contains('active')) {
+                isReplayActive = true
+            }
+            el.classList.remove('active')
+        });
+        // Adiciona ao clicado
+        this.classList.add('active');
+
+        if ((this.textContent.trim().includes('Replay') && !isReplayActive) || (!this.textContent.trim().includes('Replay') && isReplayActive)) {
+            setReplayMode();
+        }
+    });
 });
