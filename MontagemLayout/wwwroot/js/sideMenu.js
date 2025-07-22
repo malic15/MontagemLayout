7ac3eb23e2b96b9aab5f18c72f0a7d0aee1a5a07
malic15 <matheus7184@hotmail.com>
2025-07-22 15:51:58 -0300

    Backup automÃ¡tico em 2025-07-22 15:51:58

--- MontagemLayout/wwwroot/js/sideMenu.js
+++ MontagemLayout/wwwroot/js/sideMenu.js
@@ -98,3 +98,24 @@ function layoutMode(itemLayout, itemActive) {
         initializeState();
     }
 }
+
+//sidemenu2
+const toggleButton = document.getElementById("toggle-button");
+const sidebar = document.getElementById("sidebar");
+
+const openIcon = toggleButton.querySelector(".bxs-right-arrow");
+const closeIcon = toggleButton.querySelector(".bxs-left-arrow");
+
+closeIcon.style.display = "none";
+
+toggleButton.addEventListener("click", () => {
+    sidebar.classList.toggle("active");
+
+    if (sidebar.classList.contains("active")) {
+        openIcon.style.display = "none";
+        closeIcon.style.display = "block";
+    } else {
+        openIcon.style.display = "block";
+        closeIcon.style.display = "none";
+    }
+});
