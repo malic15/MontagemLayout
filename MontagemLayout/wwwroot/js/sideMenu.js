5566d9ce4629759d3748ad26ecb3230d4919cb9d
malic15 <matheus7184@hotmail.com>
2025-07-23 15:54:17 -0300

    Backup automÃ¡tico em 2025-07-23 15:54:17

--- MontagemLayout/wwwroot/js/sideMenu.js
+++ MontagemLayout/wwwroot/js/sideMenu.js
@@ -1,110 +1,110 @@
 ﻿import { setReplayMode, initializeState } from '/js/mqttWebSocket.js';
 
-var bufferHide = false;
+//var bufferHide = false;
 
-document.getElementById("sideMenuOpenBtn").onclick = function () {
-    document.getElementById("sideMenu").classList.add("open");
-};
-document.getElementById("sideMenuCloseBtn").onclick = function() {
-    document.getElementById("sideMenu").classList.remove("open");
-};
+//document.getElementById("sideMenuOpenBtn").onclick = function () {
+//    document.getElementById("sideMenu").classList.add("open");
+//};
+//document.getElementById("sideMenuCloseBtn").onclick = function() {
+//    document.getElementById("sideMenu").classList.remove("open");
+//};
 
-document.addEventListener("mousedown", function(e){
-    const sideMenu = document.getElementById("sideMenu");
-    if (
-    sideMenu.classList.contains("open") &&
-    !sideMenu.contains(e.target) &&
-    e.target.id !== "sideMenuOpenBtn"
-    ) {
-        sideMenu.classList.remove("open");
-    }
-});
+//document.addEventListener("mousedown", function(e){
+//    const sideMenu = document.getElementById("sideMenu");
+//    if (
+//    sideMenu.classList.contains("open") &&
+//    !sideMenu.contains(e.target) &&
+//    e.target.id !== "sideMenuOpenBtn"
+//    ) {
+//        sideMenu.classList.remove("open");
+//    }
+//});
 
-document.querySelectorAll('.side-menu-item').forEach(item => {
-    item.addEventListener('click', function (e) {
+//document.querySelectorAll('.side-menu-item').forEach(item => {
+//    item.addEventListener('click', function (e) {
         
-        // Remove a classe 'active' de todos
-        document.querySelectorAll('.side-menu-item').forEach(el => {
-            if (el.textContent.trim().includes('Replay')) {
-                replayButton(el, this)
-            }
-            if (el.textContent.trim().includes('Esconder Acúmulo')) {
-                hideBuffer(el, this)
-            }
-            if (el.textContent.trim().includes('Layout')) {
-                layoutMode(el, this)
-            }
-            el.classList.remove('active')
-        });
-        // Adiciona ao clicado
-        this.classList.add('active');
-    });
-});
+//        // Remove a classe 'active' de todos
+//        document.querySelectorAll('.side-menu-item').forEach(el => {
+//            if (el.textContent.trim().includes('Replay')) {
+//                replayButton(el, this)
+//            }
+//            if (el.textContent.trim().includes('Esconder Acúmulo')) {
+//                hideBuffer(el, this)
+//            }
+//            if (el.textContent.trim().includes('Layout')) {
+//                layoutMode(el, this)
+//            }
+//            el.classList.remove('active')
+//        });
+//        // Adiciona ao clicado
+//        this.classList.add('active');
+//    });
+//});
 
-function replayButton(itemReplay, itemActive) {
-    var isReplayActive = itemReplay.classList.contains('active');
-    var replayPanel = document.getElementById("replayPainel");
+//function replayButton(itemReplay, itemActive) {
+//    var isReplayActive = itemReplay.classList.contains('active');
+//    var replayPanel = document.getElementById("replayPainel");
 
-    if ((itemActive.textContent.trim().includes('Replay') && !isReplayActive) || (!itemActive.textContent.trim().includes('Replay') && isReplayActive)) {
-        setReplayMode();
-    }
-    if (itemActive.textContent.trim().includes('Replay')) {
-        replayPanel.style.setProperty('opacity', '1');
-        replayPanel.style.setProperty('pointer-events', 'auto');
-    } else {
-        replayPanel.style.setProperty('opacity', '0');
-        replayPanel.style.setProperty('pointer-events', 'none');
-    }
-}
-function hideBuffer(itemAcumulo, itemActive) {
-    if ((itemActive.textContent.trim().includes('Esconder Acúmulo') && bufferHide) || (!itemActive.textContent.trim().includes('Esconder Acúmulo') && !bufferHide)) {
-        return;
-    }
-    var buffer = document.querySelectorAll('.ball');
-    const bufferCharts = document.querySelectorAll('[id*="chartContainer"]:not([id*="chartContainerPbs"])');
-    if (bufferHide) {
-        buffer.forEach((tts, index) => {
-            tts.style.setProperty('opacity', '1');
-            tts.style.setProperty('pointer-events', 'auto');
-        });
-        if (bufferCharts.length > 0) {
-            bufferCharts.forEach((tts, index) => {
-                tts.style.setProperty('opacity', '1');
-                tts.style.setProperty('pointer-events', 'auto');
-            });
-        }
-        bufferHide = false;
-    } else {
-        buffer.forEach((tts, index) => {
-            tts.style.setProperty('opacity', '0');
-            tts.style.setProperty('pointer-events', 'none');
-        });
-        if (bufferCharts.length > 0) {
-            bufferCharts.forEach((tts, index) => {
-                tts.style.setProperty('opacity', '0');
-                tts.style.setProperty('pointer-events', 'none');
-            });
-        }
-        bufferHide = true;
-    }
-}
-function layoutMode(itemLayout, itemActive) {
-    var isLayoutActive = itemLayout.classList.contains('active');
-    var isLayoutItem = itemActive.textContent.trim().includes('Layout');
+//    if ((itemActive.textContent.trim().includes('Replay') && !isReplayActive) || (!itemActive.textContent.trim().includes('Replay') && isReplayActive)) {
+//        setReplayMode();
+//    }
+//    if (itemActive.textContent.trim().includes('Replay')) {
+//        replayPanel.style.setProperty('opacity', '1');
+//        replayPanel.style.setProperty('pointer-events', 'auto');
+//    } else {
+//        replayPanel.style.setProperty('opacity', '0');
+//        replayPanel.style.setProperty('pointer-events', 'none');
+//    }
+//}
+//function hideBuffer(itemAcumulo, itemActive) {
+//    if ((itemActive.textContent.trim().includes('Esconder Acúmulo') && bufferHide) || (!itemActive.textContent.trim().includes('Esconder Acúmulo') && !bufferHide)) {
+//        return;
+//    }
+//    var buffer = document.querySelectorAll('.ball');
+//    const bufferCharts = document.querySelectorAll('[id*="chartContainer"]:not([id*="chartContainerPbs"])');
+//    if (bufferHide) {
+//        buffer.forEach((tts, index) => {
+//            tts.style.setProperty('opacity', '1');
+//            tts.style.setProperty('pointer-events', 'auto');
+//        });
+//        if (bufferCharts.length > 0) {
+//            bufferCharts.forEach((tts, index) => {
+//                tts.style.setProperty('opacity', '1');
+//                tts.style.setProperty('pointer-events', 'auto');
+//            });
+//        }
+//        bufferHide = false;
+//    } else {
+//        buffer.forEach((tts, index) => {
+//            tts.style.setProperty('opacity', '0');
+//            tts.style.setProperty('pointer-events', 'none');
+//        });
+//        if (bufferCharts.length > 0) {
+//            bufferCharts.forEach((tts, index) => {
+//                tts.style.setProperty('opacity', '0');
+//                tts.style.setProperty('pointer-events', 'none');
+//            });
+//        }
+//        bufferHide = true;
+//    }
+//}
+//function layoutMode(itemLayout, itemActive) {
+//    var isLayoutActive = itemLayout.classList.contains('active');
+//    var isLayoutItem = itemActive.textContent.trim().includes('Layout');
 
-    if (isLayoutActive && isLayoutItem) {
-        return;
-    } else if (!isLayoutActive && isLayoutItem) {
-        initializeState();
-    }
-}
+//    if (isLayoutActive && isLayoutItem) {
+//        return;
+//    } else if (!isLayoutActive && isLayoutItem) {
+//        initializeState();
+//    }
+//}
 
 //sidemenu2
 const toggleButton = document.getElementById("toggle-button");
 const sidebar = document.getElementById("sidebar");
 
-const openIcon = toggleButton.querySelector(".bxs-right-arrow");
-const closeIcon = toggleButton.querySelector(".bxs-left-arrow");
+const openIcon = toggleButton.querySelector(".bxs-left-arrow");
+const closeIcon = toggleButton.querySelector(".bxs-right-arrow");
 
 closeIcon.style.display = "none";
 
